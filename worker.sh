7c43fce38c72b82bb76391ced2d7d1e25f0b15e3
Roig <groig@protonmail.com>
2021-11-25 17:05:47 -0500

    fix response variable

--- worker.sh
+++ worker.sh
@@ -17,13 +17,13 @@ while :; do
         echo "token is wrong"
         break
     else
-        EPISODE_ID=$(jq -r '.id' <<<$EPISODE)
+        EPISODE_ID=$(jq -r '.id' <<<$RESP)
         echo "got episode id $EPISODE_ID"
-        EPISODE_URL=$(jq -r '.url' <<<$EPISODE)
+        EPISODE_URL=$(jq -r '.url' <<<$RESP)
         echo "got episode url $EPISODE_URL"
         ffmpeg -y -i "$EPISODE_URL" -ac 1 -c:a libopus -b:a 24k -vbr on -compression_level 10 -frame_duration 60 -application voip episode.opus
         curl -H "$AUTH" -F "id=$EPISODE_ID" -F "audio=@episode.opus" $API_ENDPOINT_CONVERTED
         rm episode.opus
     fi
-    sleep 1
+    sleep 10
 done
