9d95f10fd24d2a7f32df78423363968af817a0a8
Roig <groig@protonmail.com>
2021-11-25 16:24:03 -0500

    auth

--- worker.sh
+++ worker.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
+AUTH="Authorization: Bearer $WORKER_TOKEN"
+
 while :; do
-    EPISODE=$(curl $API_ENDPOINT_NEXT | jq -r)
-    echo "got episode $EPISODE"
-    if [ "$EPISODE" = "noop" ]; then
-        echo "Nothing to do"
+    RESP=$(curl -H "$AUTH" $API_ENDPOINT_NEXT | jq -r)
+
+    # RESP can be:
+    # noop -> no episodes to convert
+    # Unauthorized -> fix your token
+    # {id: some_number, url: some url} -> get to work
+
+    echo "got response $RESP"
+    if [ "$RESP" = "noop" ]; then
+        echo "nothing to do"
+    elif [ "$RESP" = "Unauthorized" ]; then
+        echo "token is wrong"
+        break
     else
         EPISODE_ID=$(jq -r '.id' <<<$EPISODE)
         echo "got episode id $EPISODE_ID"
         EPISODE_URL=$(jq -r '.url' <<<$EPISODE)
         echo "got episode url $EPISODE_URL"
         ffmpeg -y -i "$EPISODE_URL" -ac 1 -c:a libopus -b:a 24k -vbr on -compression_level 10 -frame_duration 60 -application voip episode.opus
-        curl -F "id=$EPISODE_ID" -F "audio=@episode.opus" $API_ENDPOINT_CONVERTED
+        curl -H "$AUTH" -F "id=$EPISODE_ID" -F "audio=@episode.opus" $API_ENDPOINT_CONVERTED
         rm episode.opus
     fi
-    sleep 10
+    sleep 1
 done
