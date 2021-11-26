b0e2bfd95338d3bc41d47da519aba646c77bec8f
Yoel Torres <oneohthree@users.noreply.github.com>
2021-11-25 23:02:02 -0500

    Add functions and some required constants

--- worker.sh
+++ worker.sh
@@ -1,29 +1,57 @@
 #!/usr/bin/env bash
 
-AUTH="Authorization: Bearer $WORKER_TOKEN"
+API_HOST="https://roig.is-a.dev/podcasts"
+API_ENDPOINT_NEXT="${API_HOST}/api/next"
+API_ENDPOINT_CONVERTED="${API_HOST}/api/converted"
+WORKER_TOKEN=""
+AUTH="Authorization: Bearer ${WORKER_TOKEN}"
+TOOLS="jq ffmpeg"
+
+for tool in ${TOOLS}; do
+    type "${tool}" > /dev/null 2>&1 || echo "'${tool}' is missing"
+done
+
+get_episode() {
+    EPISODE_ID=$(jq -r '.id' <<<"${RESP}")
+    echo "Got episode ID ${EPISODE_ID}"
+    EPISODE_URL=$(jq -r '.url' <<<"$RESP")
+    echo "Got episode URL ${EPISODE_URL}"
+}
+
+convert_episode() {
+    ffmpeg -y -i "${EPISODE_URL}" \
+        -ac 1 -c:a libopus -b:a 24k -vbr on -compression_level 10 \
+        -frame_duration 60 -application voip episode.opus
+}
+
+upload_episode() {
+    curl -H "${AUTH}" -F "id=${EPISODE_ID}" \
+        -F "audio=@episode.opus" "${API_ENDPOINT_CONVERTED}"
+}
+
+clean() {
+    rm episode.opus
+}
 
 while :; do
-    RESP=$(curl -H "$AUTH" $API_ENDPOINT_NEXT | jq -r)
+    RESP=$(curl -H "${AUTH}" "${API_ENDPOINT_NEXT}" | jq -r)
 
     # RESP can be:
-    # noop -> no episodes to convert
-    # Unauthorized -> fix your token
-    # {id: some_number, url: some url} -> get to work
+    # noop -> No episodes to convert
+    # Unauthorized -> Check your token
+    # {id: some_number, url: some url} -> Get to work
 
-    echo "got response $RESP"
-    if [ "$RESP" = "noop" ]; then
-        echo "nothing to do"
-    elif [ "$RESP" = "Unauthorized" ]; then
-        echo "token is wrong"
+    echo "Got response ${RESP}"
+    if [[ "${RESP}" == "noop" ]]; then
+        echo "No episodes to convert"
+    elif [[ "${RESP}" == "Unauthorized" ]]; then
+        echo "Check your token"
         break
     else
-        EPISODE_ID=$(jq -r '.id' <<<$RESP)
-        echo "got episode id $EPISODE_ID"
-        EPISODE_URL=$(jq -r '.url' <<<$RESP)
-        echo "got episode url $EPISODE_URL"
-        ffmpeg -y -i "$EPISODE_URL" -ac 1 -c:a libopus -b:a 24k -vbr on -compression_level 10 -frame_duration 60 -application voip episode.opus
-        curl -H "$AUTH" -F "id=$EPISODE_ID" -F "audio=@episode.opus" $API_ENDPOINT_CONVERTED
-        rm episode.opus
+        get_episode && convert_episode && upload_episode && clean
+        
     fi
     sleep 10
 done
+
+exit 0
